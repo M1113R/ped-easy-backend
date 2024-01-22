57139e02a1a3b524a9d8196cb66524c3c29be977
M1113R <g234930@dac.unicamp.br>
2024-01-21 22:01:45 -0300

    feat: create product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './app/product/product.module';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: true,
       }),
     }),
+    ProductModule,
   ],
   controllers: [],
   providers: [],
